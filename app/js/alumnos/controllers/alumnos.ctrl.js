38a81f7f095d7a2703f428f8b6bafe5046fcb6f2
Julian dcf <juliandacostafaro@hotmail.com>
2017-07-21 17:48:59 -0300

    Cambios minimos

--- app/js/alumnos/controllers/alumnos.ctrl.js
+++ app/js/alumnos/controllers/alumnos.ctrl.js
@@ -1,8 +1,9 @@
 angular.module('myapp.alumnos')
-.controller('AlumnosCtrl', function($scope, $state, LoginService){
+.controller('AlumnosCtrl', function($scope, $state, LoginService, ParseTokenService ){
 
+  $scope.usuario = ParseTokenService.objetoDelToken();
 
-
+	console.log($scope.usuario);
 	$scope.logout = function() {
 		LoginService.logout()
 		.then(function(){
